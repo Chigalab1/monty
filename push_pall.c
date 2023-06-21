5a11657f222e78d45705f2c50a6720e0fba83d80
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-21 09:18:14 -0700

    push and pall opcodes

---
+++ push_pall.c
@@ -0,0 +1,74 @@
+#include "monty.h"
+
+/**
+ * push_node - a function that adds node to the stack
+ *
+ * @head: the head of stack
+ * @lcount: number of lines in stack
+ */
+
+void push_node(stack_t **head, unsigned int lcount)
+{
+	int data, i = 0, flag = 0;
+
+	if (context.arg)
+	{
+		i += (context.arg[0] == '-')
+
+		while (context.arg[i] != '\0')
+		{
+			if (context.arg[i] > 57 || context.arg[i] < 48)
+			{
+				flag = 1;
+			}
+			i++;
+		}
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", lcount);
+			fclose(context.file);
+			free(context.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", lcount);
+		fclose(context.file);
+		free(context.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	n = atoi(context.arg);
+	if (context.lifi == 0)
+		addnode(head, data);
+	else
+		addqueue(head, data);
+}
+
+
+
+/**
+ * pall - function that prints all elements of a stack
+ *
+ * @head: pointer to first node
+ * @lcount: Number of elements in the stack
+ */
+
+void pall(stack_t **head, unsigned int lcount)
+{
+        stack_t *tmp_node;
+        (void)lcount;
+
+        tmp_node = *head;
+        if (tmp_node == NULL)
+                return;
+
+        while (tmp_node)
+        {
+                printf("%d\n", tmp_node->n);
+                tmp_node = tmp_node->next;
+        }
+}
