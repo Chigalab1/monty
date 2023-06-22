cd9868ea532f7f45813a1f98a4979d5beba180f0
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-22 06:13:05 -0700

    add queue

--- stack-queue.c
+++ stack-queue.c
@@ -25,4 +25,21 @@ void queue(stack_t **head, unsigned int lcount)
 	context.lifi = 1;
 }
 
+/**
+ * add_queue - add a node to the end of a stack
+ * @n: new value to add
+ * @head: double pointer to the head of the stack
+ * Return: void
+ */
+void add_queue(stack_t **head, int n)
+{
+	stack_t *new_n, *res;
+
+	res = *head;
+	new_n = malloc(sizeof(stack_t));
+
+	if (new_n == NULL)
+	{
+		printf("Error\n");
+	}
 
