eaf8af478a2b3eaaa178bc44e8e1c802ea422a26
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-22 03:18:17 -0700

    nop opcode

---
+++ 5-nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - do nothing
+ * @head: double pointer to the head of the stack
+ * @lcount: line number
+ * Return: void
+ */
+void nop(stack_t **head, unsigned int lcount)
+{
+	(void) counter;
+	(void) head;
+}
