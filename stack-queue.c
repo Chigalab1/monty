92bbb3cc6bc1e27cd20e6b70b266f78da42197b3
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-22 05:45:53 -0700

    stack

---
+++ stack-queue.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * stack - implement the stack opcode
+ * @head: pointer to the head of the stack
+ * @lcount: line number
+ * Return: void
+ */
+void stack(stack_t **head, unsigned int lcount)
+{
+	(void)head;
+	(void)lcount;
+	context.lifi = 0;
+}
