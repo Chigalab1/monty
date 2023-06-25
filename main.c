c941f797b9b079c5539919a5a320f22bff46bf12
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-25 11:43:23 -0700

    fixed betty error

--- main.c
+++ main.c
@@ -5,48 +5,48 @@
 prog_data_t context = {NULL, NULL, NULL, 0};
 /*prog_data_t context;*/
 /**
- * main - code interpreter
- * @argc: argument count
- * @argv: arg vector
- * Return: 0
- */
+* main - code interpreter
+* @argc: argument count
+* @argv: arg vector
+* Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
-        char *content;
-        FILE *file;
-        size_t size = 0;
-        ssize_t r_line = 1;
-        stack_t *stck = NULL;
-        unsigned int lcount = 0;
+char *content;
+FILE *file;
+size_t size = 0;
+ssize_t r_line = 1;
+stack_t *stck = NULL;
+unsigned int lcount = 0;
 
-        if (argc != 2)
-        {
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
-        file = fopen(argv[1], "r");
-        context.file = file;
-        if (!file)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-                exit(EXIT_FAILURE);
-        }
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+file = fopen(argv[1], "r");
+context.file = file;
+if (!file)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
 
-        while (r_line > 0)
-        {
-                content = NULL;
-                r_line = getline(&content, &size, file);
-                context.content = content;
-                lcount++;
-                if (r_line > 0)
-                {
-                        exec_file(content, &stck, lcount, file);
-                }
-                free(content);
-        }
-        free_all_nodes(stck);
-        fclose(file);
-        return (0);
+while (r_line > 0)
+{
+content = NULL;
+r_line = getline(&content, &size, file);
+context.content = content;
+lcount++;
+if (r_line > 0)
+{
+exec_file(content, &stck, lcount, file);
+}
+free(content);
+}
+free_all_nodes(stck);
+fclose(file);
+return (0);
 }
 
